c069384b611f98262d793f3c1290acfaf7ad8cf9
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-03-22 18:36:11 -0300

    add AddAccount use case

---
+++ Domain/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Jorge Aluizio on 22/03/23.
+//
+
+import Foundation
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
