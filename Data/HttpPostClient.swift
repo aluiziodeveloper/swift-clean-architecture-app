200f2e85f02a2bcdb21a3ecded20d56ce1eae8da
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-03-22 20:29:50 -0300

    move production code to their own files

---
+++ Data/HttpPostClient.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Jorge Aluizio on 22/03/23.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?)
+}
