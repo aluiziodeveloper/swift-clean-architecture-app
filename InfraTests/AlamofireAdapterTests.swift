99b84d91b0771a35e432842ec8e979d65fc614f2
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-03-23 12:20:48 -0300

    add infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Jorge Aluizio on 23/03/23.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+
+    }
+}
