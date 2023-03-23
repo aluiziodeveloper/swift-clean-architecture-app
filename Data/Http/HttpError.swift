c0ca158d22de824dd92d2e7befa014a5561a27ad
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-03-22 21:07:41 -0300

    ensure RemoteAddAccount completes with error when HttpClient completes with error

---
+++ Data/Http/HttpError.swift
@@ -0,0 +1,12 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by Jorge Aluizio on 22/03/23.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+    case noConnectivity
+}
---
+++ Domain/Errors/DomainError.swift
@@ -0,0 +1,12 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by Jorge Aluizio on 22/03/23.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected
+}
