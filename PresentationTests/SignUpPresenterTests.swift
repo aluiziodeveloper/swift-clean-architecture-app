7fc119e7822bf404af8a4dae05f1b331ba6f3cda
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-03-30 09:42:03 -0300

    add presentation layer

---
+++ PresentationTests/SignUpPresenterTests.swift
@@ -0,0 +1,14 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by Jorge Aluizio on 30/03/23.
+//
+
+import XCTest
+
+final class SignUpPresenterTests: XCTestCase {
+    func test_() {
+        
+    }
+}
--- UseCasesIntegrationTests/AddAccountIntegrationTests.swift
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -13,9 +13,11 @@ import Domain
 class AddAccountIntegrationTests: XCTestCase {
     func test_add_account() {
         let alamofireAdapter = AlamofireAdapter()
-        let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        // let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        // let url = URL(string: "http://localhost:5050/api/signup")!
+        let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
         let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
-        let addAccountModel = AddAccountModel(name: "Test name", email: "aaa@a.com", password: "secret", passwordConfirmation: "secret")
+        let addAccountModel = AddAccountModel(name: "Test name", email: "\(UUID().uuidString)@gmail.com", password: "secret", passwordConfirmation: "secret")
         let exp = expectation(description: "waiting")
         sut.add(addAccountModel: addAccountModel) { result in
             switch result {
